77164d087fa588a5b2ffc1e8ea6b564987831b3a
zongkeli-94 <691436977@qq.com>
2019-07-25 14:18:02 +0800

    添加了DeptController

---
+++ src/main/java/com/aaa/house/controller/DeptController.java
@@ -0,0 +1,11 @@
+package com.aaa.house.controller;
+
+/**
+ * @Local com.aaa.house.controller
+ * @Author ZongKeLi
+ * @Date 2019/07/25  14:17
+ * @Version 1.0
+ */
+public class DeptController {
+
+}
